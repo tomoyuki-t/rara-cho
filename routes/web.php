52b76234ff2be130b40c71e140d76ae979371f47
tomoyuki-t <tomoyuki_takemoto@eparkgourmet.jp>
2021-10-06 11:59:29 +0900

    3 Create controller

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\WelcomeController@index');
 Route::get('contact', 'App\Http\Controllers\WelcomeController@contact');
+Route::get('about', 'App\Http\Controllers\PagesController@about');
