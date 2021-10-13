52bd35cfa3bc48335190b2f88713677fb6eaca1c
tomoyuki-t <tomoyuki_takemoto@eparkgourmet.jp>
2021-10-13 15:39:33 +0900

    20 Scope of published_at

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 //use Illuminate\Http\Request;
 use App\Models\Article;
 use App\Http\Requests\ArticleRequest;
+use Carbon\Carbon;
 
 class ArticlesController extends Controller
 {
     public function index(){
-        $articles = Article::all();
+        //$articles = Article::all();
+        //$articles = Article::orderBy('published_at', 'desc')->latest('created_at', 'desc')->get();
+        $articles = Article::latest('published_at')->latest('created_at')->published()->get();
         return view('articles.index', compact('articles'));
     }
 
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Article extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'body', 'published_at'];
     protected $dates = ['published_at'];
+    public function scopePublished($query){
+        $query->where('published_at', '<=', Carbon::now());
+    }
 }
